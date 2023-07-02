2567548cd74c0d1302ec583645192684180ee7dd
Heesoo <soyamilk0705@naver.com>
2023-07-02 22:45:56 +0900

    생성: HelloProject 파일 생성(자바 시작 테스트)

---
+++ HelloProject/src/test/hello/TestHello.java
@@ -0,0 +1,11 @@
+package test.hello;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object hi = "hello";
+		System.out.println(hi);
+		
+	}
+}
